1f039c10ea17b6f8f038d415a6fd02032701ab82
zfl19 <19aleiya@gmail.com>
2024-06-01 21:15:00 +0800

    路由基本使用

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 
 export default class About extends Component {
-
     render() {
+        console.log(this.props);
         return (
             <h1>About的内容</h1>
         )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 
 export default class Home extends Component {
-
     render() {
+        console.log(this.props);
         return (
             <h1>Home的内容</h1>
         )
