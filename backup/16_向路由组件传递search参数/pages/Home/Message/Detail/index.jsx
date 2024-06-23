edf0faa084e789b11831e3c78ad29f24eb46d851
zfl19 <19aleiya@gmail.com>
2024-06-24 01:30:45 +0800

    备份:向路由组件传递search参数

---
+++ backup/16_向路由组件传递search参数/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,31 @@
+import {Component} from "react";
+import qs from 'querystringify'
+
+const dataDetail = [
+    {id:'01', content:'hello msg1!'},
+    {id:'02', content:'hello msg2!'},
+    {id:'03', content:'hello msg3!'}
+]
+export default class Detail extends Component {
+    render() {
+        console.log(this.props)
+
+        // 接收params参数
+        // const {id, title} = this.props.match.params
+
+        // 接收search参数
+        const {search} = this.props.location
+        const {id, title} = qs.parse(search.slice(1))
+
+        const findData = dataDetail.find((obj) => {
+            return obj.id === id
+        })
+        return (
+            <ul>
+                <li>ID:{id}</li>
+                <li>TITLE:{title}</li>
+                <li>CONTENT:{findData.content}</li>
+            </ul>
+        )
+    }
+}
---
+++ backup/16_向路由组件传递search参数/pages/Home/Message/index.jsx
@@ -0,0 +1,39 @@
+import {Component} from "react";
+import {Link, Route} from "react-router-dom";
+import Detail from "./Detail";
+
+export default class Message extends Component {
+    state = {
+        msgInfo : [
+            {id:'01', title:'message1'},
+            {id:'02', title:'message2'},
+            {id:'03', title:'message3'}
+        ]
+    }
+    render() {
+        const {msgInfo} = this.state
+        return (
+            <div>
+                <ul>
+                    {
+                        msgInfo.map((msg) => {
+                            return (
+                                <li key={msg.id}>
+                                    {/*  向路由组件中传递params参数  */}
+                                    {/*<Link to={`/home/message/detail/${msg.id}/${msg.title}`}>{msg.title}</Link>*/}
+                                    {/*  向路由组件中传递search参数  */}
+                                    <Link to={`/home/message/detail/?id=${msg.id}&title=${msg.title}`}>{msg.title}</Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <hr/>
+                {/*  声明接收params参数  */}
+                {/*  <Route path="/home/message/detail/:id/:title" component={Detail}/>  */}
+                {/*  search参数无需接收,正常注册路由即可  */}
+                <Route path="/home/message/detail" component={Detail}/>
+            </div>
+        )
+    }
+}
